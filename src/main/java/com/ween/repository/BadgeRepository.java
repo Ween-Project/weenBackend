ed0c8ba194d774a4a786ebe69d5b0e17bf6f06f2
umutcff <umidza-map101@code.edu.az>
2026-07-01 02:43:28 +0400

    feat: add repository for Badge entity

---
+++ src/main/java/com/ween/repository/BadgeRepository.java
@@ -0,0 +1,15 @@
+package com.ween.repository;
+
+import com.ween.entity.Badge;
+import com.ween.enums.BadgeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BadgeRepository extends JpaRepository<Badge, String> {
+    List<Badge> findByTypeAndIsActiveTrue(BadgeType type);
+    Optional<Badge> findFirstByTypeAndIsActiveTrue(BadgeType type);
+}
