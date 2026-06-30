e332c1c0e092102810478877ec3c08f0acbd8215
umutcff <umidza-map101@code.edu.az>
2026-07-01 02:45:50 +0400

    feat: add repository for UserBadge entity

---
+++ src/main/java/com/ween/repository/UserBadgeRepository.java
@@ -0,0 +1,13 @@
+package com.ween.repository;
+
+import com.ween.entity.UserBadge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserBadgeRepository extends JpaRepository<UserBadge, String> {
+    List<UserBadge> findByUserId(String userId);
+    boolean existsByUserIdAndBadgeIdAndSpecialKey(String userId, String badgeId, String specialKey);
+}
