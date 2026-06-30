f7bd69718e5528570db0ecc9e39efda730b0aabd
umutcff <umidza-map101@code.edu.az>
2026-07-01 02:31:48 +0400

    feat: add BadgeType enum for classifications

---
+++ src/main/java/com/ween/enums/BadgeType.java
@@ -0,0 +1,11 @@
+package com.ween.enums;
+
+public enum BadgeType {
+        BRONZE,
+        SILVER,
+        GOLD,
+        EVENT_CATEGORY,
+        MONTHLY_WINNER,
+        STREAK,
+        ANNIVERSARY
+}
