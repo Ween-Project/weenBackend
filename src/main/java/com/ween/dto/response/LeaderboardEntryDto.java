e1c90664332d1dfe6489d712d882a4b99dcf988c
Mansura <badalovamansura40@gmail.com>
2026-07-07 17:26:30 +0400

    feat : edit LeaderboardEntryDto

--- src/main/java/com/ween/dto/response/LeaderboardEntryDto.java
+++ src/main/java/com/ween/dto/response/LeaderboardEntryDto.java
@@ -1,17 +1,31 @@
 package com.ween.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class LeaderboardEntryDto {
     private Integer rank;
+    private String userId;
     private String username;
+    private String fullName;
     private String profilePhotoUrl;
     private Integer coins;
-}
+    private String university;
+    private String major;
+    private String course;
+    private String skills;
+    private String interests;
 
+    public LeaderboardEntryDto(Integer rank, String username, String profilePhotoUrl, Integer coins) {
+        this.rank = rank;
+        this.username = username;
+        this.profilePhotoUrl = profilePhotoUrl;
+        this.coins = coins;
+    }
+}
