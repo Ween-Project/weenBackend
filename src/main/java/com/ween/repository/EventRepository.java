b2b0b0d70a8014f397e6960c7843882d614ff89c
umutcff <umidza-map101@code.edu.az>
2026-07-08 01:41:13 +0400

    feat: adding new method(countByStatus) to EventRepository

--- src/main/java/com/ween/repository/EventRepository.java
+++ src/main/java/com/ween/repository/EventRepository.java
@@ -39,6 +39,8 @@ public interface EventRepository extends JpaRepository<Event, String>, JpaSpecif
     
     List<Event> findByStatusOrderByStartDateAsc(EventStatus status);
 
+    long countByStatus(EventStatus status);
+
     @Query("SELECT e FROM Event e WHERE e.startDate BETWEEN :now AND :end AND e.status = :status")
     List<Event> findEventsStartingBetween(@Param("now") LocalDateTime now, @Param("end") LocalDateTime end, @Param("status") EventStatus status);
 
